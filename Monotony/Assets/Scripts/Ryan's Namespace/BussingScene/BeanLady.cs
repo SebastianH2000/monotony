53db5071f9463e0bba85c3e3fbffc9440ee3fd91
ryancheng44 <ryancheng44@gmail.com>
2023-08-08 15:19:18 -0400

    Revert "Merge branch 'main' of https://github.com/SebastianH2000/monotony"
    This reverts commit 150c392e651bb96292ddeefaca0ce510fce1c163, reversing
    changes made to e49ae635a8bc0f5fb94a448af43e0bd1d2952a96.

--- Monotony/Assets/Scripts/Ryan's Namespace/BussingScene/BeanLady.cs
+++
@@ -1,9 +0,0 @@
-public class BeanLady : Customer
-{
-    // Start is called before the first frame update
-    protected override void Start() {
-        order = Food.Beans;
-
-        transform.GetChild(0).GetComponentInChildren<OrderRequest>().SetFood(order);
-    }
-}
