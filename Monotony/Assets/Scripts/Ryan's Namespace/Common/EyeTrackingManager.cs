6d94906a6c5a7359652fb1233c727e86f043966d
ryancheng44 <ryancheng44@gmail.com>
2023-08-10 11:44:49 -0400

    eye tracking manager update

--- Monotony/Assets/Scripts/Ryan's Namespace/Common/EyeTrackingManager.cs
+++ Monotony/Assets/Scripts/Ryan's Namespace/Common/EyeTrackingManager.cs
@@ -5,81 +5,75 @@ using SebastiansNamespace;
 namespace RyansNamespace {
     public class EyeTrackingManager : MonoBehaviour
     {
-        //[SerializeField] private PlayerHealth player;
-
         private float offset;
         private bool isConnected = false;
-        [SerializeField] private GameObject visionLocation;
-        [SerializeField] private float checkRadius; 
+        [SerializeField] private Transform visionLocation;
+        [SerializeField] private float checkRadius;
+        private bool inEditor = false;
 
         // Start is called before the first frame update
         void Start()
         {
+            offset = Camera.main.transform.position.z;
+
+            if (Application.isEditor) {
+                inEditor = true;
+                return;
+            }
+
             if (TobiiAPI.IsConnected)
                 isConnected = true;
             else
                 isConnected = false;
-
-            offset = Camera.main.transform.position.z;
-            //visionLocation.transform.localScale = new Vector3(checkRadius,checkRadius,1);
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            visionLocation.transform.position = new Vector3(worldPos.x,worldPos.y,-5);
-            if (!isConnected || TobiiAPI.GetUserPresence() != UserPresence.Present)
-                //return;
+            Vector3 screenPos;
 
-            //GazePoint gazePoint = TobiiAPI.GetGazePoint();
-            //if (gazePoint.IsValid && GameObject.Find("IntroCard"))
-            if (true)
-            {
-                //Vector3 screenPos = gazePoint.Screen;
-                //screenPos.z = -offset;
-                //Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
-                worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-                //visionLocation.transform.position = worldPos;
-
-                //Collider2D collider = Physics2D.Raycast(worldPos, Vector2.zero).collider;
-
-                int returnDist = 0;
-                for (int i = 1; i < 5; i++) {
-                    Collider2D collider = Physics2D.OverlapCircle(worldPos, checkRadius/Mathf.Pow(2,i));
-                    if (collider != null) {
-                        if (collider.CompareTag("Monster")) {
-                            if (i > returnDist)
-                                returnDist = i;
-                                SavePlayerData.monsterDistance = i;
-                        }
-                    }
-                }
-                if (returnDist > 0) {
-                    Debug.Log(returnDist);
-                    //float distance = Vector2.Distance(worldPos, collider.transform.positon);
-                    SavePlayerData.sanity = Mathf.Clamp(SavePlayerData.sanity - (Time.deltaTime / 30f * (returnDist)), 0f, 1f);
-                    if (SavePlayerData.sanity <= 0) {
-                        GameObject.Find("FadeOut").GetComponent<FadeOut>().isFading = true;
-                    }
-                    SavePlayerData.lookingAtMonster = true;
+            if (inEditor) {
+                screenPos = Input.mousePosition;
+            } else {
+                if (!isConnected || TobiiAPI.GetUserPresence() != UserPresence.Present)
                     return;
-                }
 
-                /*Collider2D collider = Physics2D.OverlapCircle(worldPos, checkRadius);
+                GazePoint gazePoint = TobiiAPI.GetGazePoint();
+                if (gazePoint.IsValid) {
+                    screenPos = gazePoint.Screen;
+                } else {
+                    screenPos = Input.mousePosition;
+                }
+            }
+
+            screenPos.z = -offset;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            visionLocation.position = new Vector3(worldPos.x, worldPos.y, 0f);
+
+            int inverseDistance = 0;
+
+            for (int i = inverseDistance; i < 5; i++) {
+                Collider2D collider = Physics2D.OverlapCircle(worldPos, checkRadius / Mathf.Pow(2, i));
+
                 if (collider != null) {
                     if (collider.CompareTag("Monster")) {
-                        float distance = Vector2.Distance(worldPos, collider.transform.positon);
-                        SavePlayerData.sanity = Mathf.Clamp(SavePlayerData.sanity - Time.deltaTime / 5f, 0f, 1f);
-                        SavePlayerData.lookingAtMonster = true;
-                        return;
+                        if (i > inverseDistance)
+                            inverseDistance = i;
+                            SavePlayerData.monsterDistance = i;
                     }
-                }*/
-
-                SavePlayerData.lookingAtMonster = false;
-                SavePlayerData.monsterDistance = 0;
+                }
             }
+
+            if (inverseDistance > 0) {
+                SavePlayerData.sanity = Mathf.Clamp(SavePlayerData.sanity - (Time.deltaTime / 30f * (inverseDistance)), 0f, 1f);
+                if (SavePlayerData.sanity <= 0)
+                    GameObject.Find("FadeOut").GetComponent<FadeOut>().isFading = true;
+                SavePlayerData.lookingAtMonster = true;
+                return;
+            }
+
+            SavePlayerData.lookingAtMonster = false;
+            SavePlayerData.monsterDistance = 0;
         }
     }
 }
